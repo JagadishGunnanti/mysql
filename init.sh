e550e57f64514d39d3e0d97a260710f95a8fa449
root <root@ip-172-31-38-130.us-east-2.compute.internal>
2018-03-05 20:05:09 +0000

    Updated init script to parse json data using shell

--- init.sh
+++ init.sh
@@ -1,17 +1,53 @@
 #!/bin/bash
-mysql -uroot -pmysqlpassword mysqldb <<EOF
-EOF
-echo "dumping json data"
+
+export MYSQL_PWD=$MYSQL_ROOT_PASSWORD
+
 while true
 do
-curl --create-dirs -sSLo /tmp/json/test.json http://freegeoip.net/json/
-mysql -uroot -pmysqlpassword mysqldb << EOF
-CREATE TABLE IF NOT EXISTS jsons_test(
-       ID INT AUTO_INCREMENT NOT NULL UNIQUE,
-               json_data json);
-               LOAD DATA LOCAL INFILE '/tmp/json/test.json'
-               INTO table jsons_test(json_data);
+  echo "Getting Data from GEOIP Portal"
+  curl --create-dirs -sSLo /tmp/json/test1.json http://freegeoip.net/json/
+
+function getPropValue() {
+  KEY=$1;
+  num=$2;
+  awk -F "[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p;
+}
+
+echo "Parsing GEOPIP Data";
+ip=$(cat /tmp/json/test1.json|getPropValue ip);
+country_code=$(cat /tmp/json/test1.json|getPropValue country_code);
+country_name=$(cat /tmp/json/test1.json|getPropValue country_name);
+region_code=$(cat /tmp/json/test1.json|getPropValue region_code);
+region_name=$(cat /tmp/json/test1.json|getPropValue region_name);
+city=$(cat /tmp/json/test1.json|getPropValue city);
+zip_code=$(cat /tmp/json/test1.json|getPropValue zip_code);
+time_zone=$(cat /tmp/json/test1.json|getPropValue time_zone);
+latitude=$(cat /tmp/json/test1.json|getPropValue latitude);
+longitude=$(cat /tmp/json/test1.json|getPropValue longitude);
+metro_code=$(cat /tmp/json/test1.json|getPropValue metro_code);
+
+echo "Inserting parsed Data into mysql database";
+mysql -uroot mysqldb << EOF
+CREATE TABLE IF NOT EXISTS geoipdata
+(
+SNO INT AUTO_INCREMENT NOT NULL UNIQUE,
+ip VARCHAR(15),
+country_code VARCHAR(20),
+country_name VARCHAR(20),
+region_code VARCHAR(20),
+region_name VARCHAR(20),
+city VARCHAR(20),
+zip_code INT,
+time_zone VARCHAR(20),
+latitude DECIMAL(10,8),
+longitude DECIMAL(10,8),
+metro_code BIGINT UNSIGNED
+);
+
+INSERT INTO geoipdata SET ip="$ip",country_code="$country_code",country_name="$country_name",region_code="$region_code",region_name="$region_name",city="$city",zip_code="$zip_code",time_zone="$time_zone",latitude="$latitude",longitude="$longitude",metro_code="$metro_code";
 EOF
-rm -rf /tmp/json/
-sleep 60
-done
+
+echo "done";
+rm -rf /tmp/json
+sleep 60;
+done;
